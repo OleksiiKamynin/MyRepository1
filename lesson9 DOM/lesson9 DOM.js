ef080bd14d38739b5a1aa672fa7ec2154c970a39
OleksiiKamynin <141737024+OleksiiKamynin@users.noreply.github.com>
2023-09-13 18:38:23 +0300

    Add files via upload

---
+++ lesson9 DOM/lesson9 DOM.js
@@ -0,0 +1,262 @@
+let newDiv = document.createElement('div');
+newDiv.innerText = 'somelonglongtext'
+
+let wrap1 = document.getElementsByClassName('wrap')[0];
+wrap1.appendChild(newDiv)
+wrap1.appendChild(newDiv.cloneNode(true));
+
+let alpha1 = document.getElementsByClassName('alpha')[0];
+alpha1.appendChild(newDiv)
+alpha1.appendChild(newDiv.cloneNode(true));
+
+let collapse1 = document.getElementsByClassName('collapse')[0];
+collapse1.appendChild(newDiv)
+collapse1.appendChild(newDiv.cloneNode(true));
+
+let beta1 = document.getElementsByClassName('beta')[0];
+beta1.appendChild(newDiv)
+beta1.appendChild(newDiv.cloneNode(true));
+
+//______________________________________//
+
+
+//- Є масив:
+//    ['Main','Products','About us','Contacts']
+//Зробити ul в середині якої будуть лежати елементи масиву (кожен в своєму li)
+
+let mainInfo = [" Main", " Products", " About us", " Contacts"];
+html = '<ul>';
+mainInfo.forEach(function(item, i, arr) {
+    html += '<li>'+item+'</li>';
+});
+html += '</ul>'
+
+document.write(html);
+
+
+//_______________________________________//
+
+//- Є масив
+//let coursesAndDurationArray = [
+//    {title: 'JavaScript Complex', monthDuration: 5},
+//    {title: 'Java Complex', monthDuration: 6},
+//    {title: 'Python Complex', monthDuration: 6},
+//    {title: 'QA Complex', monthDuration: 4},
+//    {title: 'FullStack', monthDuration: 7},
+//    {title: 'Frontend', monthDuration: 4}
+//];
+//Для кожного елементу масиву зробити блок в якому вивести інформацію про title та monthDuration
+//Завдання робити через цикли.
+
+
+let coursesAndDurationArray = [
+   {title: 'JavaScript Complex', monthDuration: 5},
+   {title: 'Java Complex', monthDuration: 6},
+   {title: 'Python Complex', monthDuration: 6},
+   {title: 'QA Complex', monthDuration: 4},
+   {title: 'FullStack', monthDuration: 7},
+   {title: 'Frontend', monthDuration: 4}
+];
+
+for(const courses of coursesAndDurationArray){
+   let div = document.createElement('div');
+   div.classList.add('product')
+
+    let h2 = document.createElement('h2');
+    h2.innerText = `title - ${courses.title} .monthDuration - ${courses.monthDuration}`
+    div.append(h2);
+
+    document.body.appendChild(div);
+}
+
+//___________________________________//
+
+//- Є масив
+//let coursesAndDurationArray = [
+//    {title: 'JavaScript Complex', monthDuration: 5},
+//    {title: 'Java Complex', monthDuration: 6},
+//    {title: 'Python Complex', monthDuration: 6},
+//    {title: 'QA Complex', monthDuration: 4},
+//    {title: 'FullStack', monthDuration: 7},
+//    {title: 'Frontend', monthDuration: 4}
+//];
+//За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,  в якому буде <h1 class='heading'>  з title  елементу, та <p class='description'> з monthDuration елементу
+//Завдання робити через цикли
+
+
+for(const item of coursesAndDurationArray){
+    let div2 = document.createElement("div2")
+    div2.classList.add('item')
+
+    let h2 = document.createElement('h2');
+    h2.innerText = `title - ${item.title}`
+    div2.append(h2);
+
+    let p2 = document.createElement("p2");
+    p2.innerText = `monthDuration - ${item.monthDuration}`
+    div2.append(p2);
+
+    document.body.appendChild(div2);
+}
+
+
+//______________________________________________//
+
+//- є масив simpsons, котрий лежить в arrays.js (на цей момент ви вже знаєте де він знаходиться)
+// Проітерувати його, створиши для кожного об'єкту  масиву <div class='member'> та наповнити його данними з об'єкту.
+// Якщо людською мовою: під кожного члена родини зробити блок та наповнити його інформацією з цього об'єкту
+
+
+let simpsons = [
+    {
+        name: 'Bart',
+        surname: 'Simpson',
+        age: 10,
+        info: 'Бартолом\'ю ДжоДжо «Барт» Сімпсон (англ. Bartholomew JoJo «Bart» Simpson) — один із головних героїв мультиплікаційного серіалу Сімпсони. Барт — найстарша дитина Гомера і Мардж Сімпсон. У нього також є дві молодші сестри — Ліса і Меґґі. Барт є втіленням образу бешкетника та посереднього учня у школі. Разом зі своїм батьком Барт є одним із найвідоміших персонажів у цьому серіалі.',
+        photo: 'https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png'
+    },
+    {
+        name: 'Homer',
+        surname: 'Simpson',
+        age: 40,
+        info: 'Гомер Джей Сімпсон (англ. Homer Jay Simpson) — один із головних героїв мультсеріалу «Сімпсони». Гомер — грубий і неввічливий батько родини, він має очевидні вади: товстий, лисий і не дуже розумний. Нерідко він поводиться як блазень, абсурдно, егоїстично і нетактовно, але все ж лишається симпатичним.',
+        photo: 'http://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'
+    },
+    {
+        name: 'Marge',
+        surname: 'Simpson',
+        age: 38,
+        info: 'Ма́рджори Жакли́н «Мардж» Си́мпсон (в девичестве Бувье́) (англ. Marjorie Jacqueline «Marge» Simpson) — постоянный персонаж мультипликационного сериала «Симпсоны», её озвучивает Джулия Кавнер. Обычно носит зелёное платье, красные балетки, на шее — ожерелье из искусственного жемчуга и ездит на оранжевом универсале. У неё шикарные синие волосы, которые она обычно собирает в очень высокую причёску. Глаза цвета ореха (19s6e). Основное занятие — домохозяйка, большую часть времени проводит в заботе о доме, детях и Гомере. Образ Мардж копирует стереотип провинциальной американской домохозяйки 50-х годов. Мардж — единственный член семьи, посещающий церковь добровольно. Старается поддерживать нравственность не только своей семьи, но и всего города. Отлично готовит, особенно славятся её свиные отбивные и зефир. Любимое блюдо — лапша с маслом.',
+        photo: 'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'
+    },
+    {
+        name: 'Lisa',
+        surname: 'Simpson',
+        age: 9,
+        info: 'Ли́за Мари́ Си́мпсон (англ. Lisa Marie Simpson) — героиня мультипликационного сериала «Симпсоны». Средний ребёнок в семье, восьмилетняя девочка, выделяющаяся среди остальных Симпсонов прежде всего своим умом и рассудительностью.',
+        photo: 'https://upload.wikimedia.org/wikipedia/ru/e/ec/Lisa_Simpson.png'
+    },
+    {
+        name: 'Maggie',
+        surname: 'Simpson',
+        age: 1,
+        info: 'Ма́ргарет Эвелин «Мэ́гги» Си́мпсон (англ. Margaret Evelyn “Maggie” Simpson) — персонаж мультсериала «Симпсоны». Впервые появилась на телевидении в шоу Трейси Ульман, в короткометражке Good Night (англ.)русск. 19 апреля 1987 года. Мэгги была придумана и разработана карикатуристом Мэттом Грейнингом, пока он ждал встречи с Джеймсом Л. Бруксом. Названа в честь младшей сестры Грейнинга. После появления в шоу Трейси Ульман, через три года семья Симпсонов получила собственный сериал на телеканале Fox, дебют произошёл 17 декабря 1989 года.',
+        photo: 'https://upload.wikimedia.org/wikipedia/ru/9/9d/Maggie_Simpson.png'
+    },
+];
+for(const familyMembers of simpsons){
+    let div3 = document.createElement('div3');
+    div3.classList.add('member')
+
+    let p3 = document.createElement("p3");
+    p3.innerText = `name - ${familyMembers.name}, surname - ${familyMembers.surname}, age - ${familyMembers.age}, info - ${familyMembers.info}`
+
+    let img = document.createElement('img')
+    img.src = familyMembers.photo;
+    img.alt = familyMembers.name
+
+    div3.append(img, p3)
+    document.body.appendChild(div3);
+}
+
+
+//________________________________________________//
+
+//Цикл в циклі
+//- Є масив coursesArray котрий лежить в arrays.js (на цей момент ви вже знаєте де він знаходиться)
+
+//Створити для кожного елементу масиву свій блок, блок розділити блоками, в яких будуть зберігатись значення окремих властивостей, для властивості modules зробити список з елементами
+//Приклад структири знаходиться у файлі example.png який лежить в папці з поточним фйлом
+
+let coursesArray = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'java core',
+            'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'python core',
+            'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'react',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'node.js',
+            'python',
+            'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];
+
+for(const techCourses of coursesArray) {
+    let div4 = document.createElement('div4');
+    div4.classList.add('subject')
+
+    let p4 = document.createElement('p4');
+    p4.innerText = `title - ${techCourses.title}, monthDuration - ${techCourses.monthDuration}, hourDuration - ${techCourses.hourDuration}`
+
+    for (const moduleNames of coursesArray[modules]){
+        let div5 = document.createElement('div5')
+        div5.classList.add('moduleTitles')
+
+        let p5 = document.createElement('p5')
+        p5.innerText = `modules - ${moduleNames.modules}`
+
+    div4.append(p4, p5)
+    document.body.append(div4);
+
+}}
+
+
+
+
